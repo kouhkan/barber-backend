5bfe3fb96c3947e652382d57aefc916c127cc071
kouhkan <amirkouhkan1@gmail.com>
2021-11-21 14:09:25 +0330

    add user and barber list reservations urls

--- apps/reservations/api/v1/urls.py
+++ apps/reservations/api/v1/urls.py
@@ -6,5 +6,13 @@ from . import views
 app_name = 'reservations'
 
 urlpatterns = [
-    path('time/', views.reserve_time, name='reserve_time'),
+    path('time/',
+         views.reserve_time,
+         name='reserve_time'),
+    path('barber_list/',
+         views.barber_reservations_list,
+         name='barber_reservations_list'),
+    path('user_list/',
+         views.user_reservations_list,
+         name='user_reservations_list'),
 ]
